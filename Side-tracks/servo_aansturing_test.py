29cf229877f31b8ba363118290a96f01fa0cc4c9
EmiTree <emilyw003@outlook.com>
2026-05-04 12:42:45 +0200

    modified:   Side-tracks/servo_aansturing_test.py

--- Side-tracks/servo_aansturing_test.py
+++ Side-tracks/servo_aansturing_test.py
@@ -22,15 +22,20 @@ def use_keys():
         servo_2_pos = 180
     if keyboard.is_pressed("d"):
         servo_2_pos = 0
-   
+    if keyboard.is_pressed("z"):
+        servo_2_pos = 90
+    if keyboard.is_pressed("x"):
+        servo_2_pos -= 1
+    if keyboard.is_pressed("c"):
+        servo_2_pos += 1
 while True:
     use_keys()
 
     #print(ser.readline().decode("utf-8"))
-    servo_1_command = f"A:{servo_1_pos}"
+    servo_1_command = f"A:{servo_1_pos}\n"
     ser.write(servo_1_command.encode("utf-8"))
 
-    servo_2_command = f"B:{servo_2_pos}"
+    servo_2_command = f"B:{servo_2_pos}\n"
     ser.write(servo_2_command.encode("utf-8"))
     print(f"Sent commands: {servo_1_command}, {servo_2_command}")
     time.sleep(.1)    
